6c92a64a5a710c8e17d18834aad54c69aa2a9416
Muffiuss <ikpemuffius@yahoo.com>
2023-07-11 00:48:25 -0400

    deploying to QA

--- sh.groovy
+++ sh.groovy
@@ -20,8 +20,16 @@ def devDeploy() {
     def image = 'muffius/demo-repo:jma-3.0'
     def stopCmd = "docker stop \$(docker ps -q --filter ancestor=${image}) || true && docker rm \$(docker ps -a -q --filter ancestor=${image}) || true"
     def dockerCmd = "docker run -d -p 8080:8080 ${image}"
-    sshagent(['dev-key']) {
-        sh "ssh -o StrictHostKeyChecking=no ubuntu@44.202.39.78 '${stopCmd} && ${dockerCmd}'"
+
+    withCredentials([string(credentialsId: 'docker-hub-access-token', variable: 'DOCKERHUB_ACCESS_TOKEN')]) {
+        sshagent(['dev-key']) {
+            sh """
+            ssh -o StrictHostKeyChecking=no ubuntu@44.202.39.78 '
+                docker login -u muffius -p "${DOCKERHUB_ACCESS_TOKEN}" &&
+                ${stopCmd} &&
+                ${dockerCmd}'
+            """
+        }
     }
 }
 
